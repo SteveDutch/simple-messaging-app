f567f84c81ca4cf20696c0ee40b40ded22f49c23
SteveDutch <steholl@posteo.org>
2023-01-18 20:08:41 +0100

    draft controller/html for channel & welcome

--- src/main/java/com/stevedutch/assignment14/web/ChannelController.java
+++ src/main/java/com/stevedutch/assignment14/web/ChannelController.java
@@ -1,8 +1,17 @@
 package com.stevedutch.assignment14.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChannelController {
+
+	@GetMapping("/channel")
+	public String showChannel(ModelMap model) {
+
+		model.put("user", model);
+		return "/channel";
+	}
 
 }
--- src/main/java/com/stevedutch/assignment14/web/UserController.java
+++ src/main/java/com/stevedutch/assignment14/web/UserController.java
@@ -1,8 +1,17 @@
 package com.stevedutch.assignment14.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+	@GetMapping("/welcome")
+	public String welcomeUser(ModelMap model) {
+
+		model.put("user", model);
+		return "/welcome";
+	}
 
 }
